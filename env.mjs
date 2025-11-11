0f7038371cc9c898fdf9ebac18a351ffe6121619
hankpharris <hopharris@wpi.edu>
2025-11-11 10:01:31 -0500

    Added db, auth, admin panel

--- env.mjs
+++ env.mjs
@@ -7,9 +7,11 @@ export const env = createEnv({
       .enum(["true", "false"])
       .optional()
       .transform((value) => value === "true"),
+    DATABASE_URL: z.string().url(),
   },
   client: {},
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 })
